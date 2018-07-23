f1344fe282bb86f604f69dc3b025f8b6a17d167a
yinshicong <yinshicong@zchz.com>
2018-07-23 20:36:09 +0800

    。。。

---
+++ src/yilaidaozhi/Architect.java
@@ -0,0 +1,8 @@
+package yilaidaozhi;
+
+/**
+ * Created by Administrator on 2018/7/23 0023.
+ */
+public interface Architect extends Language{
+    public void architecture();
+}
--- src/yilaidaozhi/Programmer.java
+++ src/yilaidaozhi/Programmer.java
@@ -3,7 +3,7 @@ package yilaidaozhi;
 
 public class Programmer {
     //高层逻辑依赖了底层的原子逻辑，此时两个类耦合了，没有任何扩展言。
-    public void programming(Language java){
-        java.say();
+    public void programming(Language language){
+        language.say();
     }
 }
